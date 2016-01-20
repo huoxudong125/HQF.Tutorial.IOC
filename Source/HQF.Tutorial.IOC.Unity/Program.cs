d63704494b14cd720d22ebc6aaa054381a02b271
huoxudong125 <423huo@163.com>
2016-01-20 17:36:01 +0800

    Fix the Unity Demo Project ,Run right.

--- Source/HQF.Tutorial.IOC.Unity/Program.cs
+++ Source/HQF.Tutorial.IOC.Unity/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HQF.Tutorial.IOC.Common.Interface;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
 
 namespace HQF.Tutorial.IOC.Unity
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            //Load configuration from app.config.
+            IUnityContainer container = new UnityContainer();
+            container.LoadConfiguration();
+
+            //Resolve the class
+            var myclass = container.Resolve<MyClass>();
+            myclass.CallPrint();
+
+            Console.Read();
+        }
+    }
+
+    public class MyClass
+    {
+        public InterfaceA ComponentA { get; set; }
+
+        //Dependencies Inject,using constructor.
+        public MyClass(InterfaceA a)
+        {
+            ComponentA = a;
+        }
+
+        public void CallPrint()
+        {
+            if (ComponentA != null)
+            {
+                ComponentA.Print();
+            }
+            else
+            {
+                Console.WriteLine("The ComponentA is null!");
+            }
         }
     }
 }
