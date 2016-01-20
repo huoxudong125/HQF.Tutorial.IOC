aeec442ace6ace790e629f47c33a2cf3fd64d44c
huoxudong125 <423huo@163.com>
2016-01-20 16:35:48 +0800

    Add Init Solution

---
+++ Source/HQF.Tutorial.IOC.Autofac/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tutorial.IOC.Autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Source/HQF.Tutorial.IOC.Unity/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tutorial.IOC.Unity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
