075350c2fcdc32ad0a5a001c937187d9e8ba492b
huoxudong125 <423huo@163.com>
2016-01-20 16:45:05 +0800

    Add Common Interface & Implement

---
+++ Source/Common/HQF.Tutorial.IOC.Common.Implement/ImplementA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HQF.Tutorial.IOC.Common.Interface;
+
+namespace HQF.Tutorial.IOC.Common.Implement
+{
+    public class ImplementA:InterfaceA
+    {
+        public void Print()
+        {
+            Console.WriteLine("This is the Implement A,Which implement the interface InterfaceA");
+        }
+    }
+}
---
+++ Source/Common/HQF.Tutorial.IOC.Common.Interface/InterfaceA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tutorial.IOC.Common.Interface
+{
+    public interface InterfaceA
+    {
+         void Print();
+    }
+}
