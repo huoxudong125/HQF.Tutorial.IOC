3d6ce8120a05cab9d262baf6ef6aa90c28d4ac50
huoxudong125 <423huo@163.com>
2016-01-21 09:37:12 +0800

    Fix Ninject runing correct.

--- Source/HQF.Tutorial.IOC.Ninject/Program.cs
+++ Source/HQF.Tutorial.IOC.Ninject/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using HQF.Tutorial.IOC.Common.Interface;
+using Ninject;
+using Ninject.Modules;
 
 namespace HQF.Tutorial.IOC.Ninject
 {
@@ -10,6 +14,40 @@ namespace HQF.Tutorial.IOC.Ninject
     {
         static void Main(string[] args)
         {
+            IKernel kernel = new StandardKernel();
+       
+        kernel.Load("yourXmlFileName.xml");
+               bool ismodule = kernel.HasModule("myXmlConfigurationModule");//To Check The module 
+
+        var a=kernel.Get<InterfaceA>();
+            a.Print();
+
+            Console.Read();
+        }
+
+       
+    }
+
+    public class MyClass
+    {
+        public InterfaceA ComponentA { get; set; }
+
+        //Dependencies Inject,using constructor.
+        public MyClass(InterfaceA a)
+        {
+            ComponentA = a;
+        }
+
+        public void CallPrint()
+        {
+            if (ComponentA != null)
+            {
+                ComponentA.Print();
+            }
+            else
+            {
+                Console.WriteLine("The ComponentA is null!");
+            }
         }
     }
 }
