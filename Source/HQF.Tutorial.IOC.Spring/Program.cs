a42e9d33d5a3985fd7958037e0ce1dcd3eaca4fe
Frank <qf.huo@outlook.com>
2016-12-05 00:19:54 +0800

    Add sprint demo, need to fix

---
+++ Source/HQF.Tutorial.IOC.Spring/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HQF.Tutorial.IOC.Common.Interface;
+
+namespace HQF.Tutorial.IOC.Spring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.Read();
+        }
+
+    }
+
+    public class MyClass
+    {
+        public InterfaceA ComponentA { get; set; }
+
+        //Dependencies Inject,using constructor.
+        public MyClass(InterfaceA a)
+        {
+            ComponentA = a;
+        }
+
+        public void CallPrint()
+        {
+            if (ComponentA != null)
+            {
+                ComponentA.Print();
+            }
+            else
+            {
+                Console.WriteLine("The ComponentA is null!");
+            }
+        }
+    }
+
+
+}
