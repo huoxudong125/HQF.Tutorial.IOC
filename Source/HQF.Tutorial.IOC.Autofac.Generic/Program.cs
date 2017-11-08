ff75f8f4430fdb9ca313d80873b9fb8d59539746
huoxudong125 <423huo@163.com>
2017-11-08 13:43:44 +0800

    Add Geniric Type Test for AutoFac

---
+++ Source/HQF.Tutorial.IOC.Autofac.Generic/Program.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tutorial.IOC.Autofac.Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<OrderPaidEventHandler>().As<IEventHander<OrderPaidEvent>>();
+
+            var container=  builder.Build();
+
+
+
+            //var ope= container.Resolve<IEventHander<OrderPaidEvent>>();
+
+            // Console.WriteLine(ope);
+            //var e=(IEvent)new OrderPaidEvent();
+            var e = new OrderPaidEvent();
+
+
+
+            GetEvent(container,e);
+
+            Console.Read();
+
+
+        }
+
+        public static void GetEvent<T>(IContainer container, T t)  where T:IEvent {
+
+
+            var ope = container.Resolve<IEventHander<T>>();
+
+            Console.WriteLine(ope);
+
+        }
+    }
+}
